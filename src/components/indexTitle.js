f4c348aa4bedb911438563fb1e37dacf9edbd968
Eric Weischedel <eweischedel@scheduleengine.com>
2020-07-06 17:40:56 -0400

    change component to class

--- src/components/indexTitle.js
+++ src/components/indexTitle.js
@@ -1,11 +1,93 @@
 import React from "react"
+import "../styles/indexTitle.css"
 
-const styles = {
-  fontSize: '5em'
+const titles = [
+  "Problem Solver",
+  "Team Player",
+  "Coding Ninja",
+  "Hard Worker",
+  "'21 Grad"
+];
+
+class IndexTitle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      opacity: 1,
+      title: "Problem Solver"
+    };
+    const interval = setInterval(() => {
+      this.changeTitle()
+    }, 4000);
+  }
+
+  getRandomTitle() {
+    var newTitle = this.state.title;
+    while (newTitle === this.state.title) {
+      newTitle = titles[Math.floor(Math.random() * (titles.length - 1))];
+    }
+    return newTitle;
+  }
+
+  sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+  }
+
+  async changeTitle() {
+    this.setState({ opacity: 0 });
+    await this.sleep(1000);
+    const newTitle = this.getRandomTitle();
+    this.setState({ title: newTitle });
+    this.setState({ opacity: 1 })
+  }
+
+  render() {
+    return (
+      <h1 className="title">
+        Software Developer
+        <br />& <span style={{ opacity: this.state.opacity }}>{this.state.title}</span>
+      </h1>
+    )
+  }
 }
 
-const IndexTitle = () => (
-  <h1 style={styles}>Software Developer<br/>& Coding Ninja</h1>
-)
+// const IndexTitle = () => {
+//   const [title, setTitle] = useState("Problem Solver");
+//   const [opacity, setOpacity] = useState(1);
+//   function getRandomTitle() {
+//     var newTitle = title;
+//     while (newTitle === title) {
+//       newTitle = titles[Math.floor(Math.random() * (titles.length - 1))];
+//       console.log(
+//         `title: ${title} \n newTitle: ${newTitle} \n newTitle === title: ${
+//           newTitle === title
+//         }`
+//       )
+//     }
+//     return newTitle;
+//   }
+//   function sleep(ms) {
+//     return new Promise(resolve => window.setTimeout(resolve, ms))
+//   }
+//   async function changeTitle() {
+//     setOpacity(0);
+//     await sleep(1000);
+//     const newTitle = getRandomTitle();
+//     setTitle(newTitle);
+//     setOpacity(1);
+//   }
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       changeTitle()
+//     }, 4000)
+//     return () => clearInterval(interval)
+//   }, []);
+//   return (
+//     <h1 onClick={changeTitle} className="title">
+//       Software Developer
+//       <br />& <span style={{opacity: opacity}}>{title}</span>
+//     </h1>
+//   )
+// };
 
 export default IndexTitle
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,14 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import IndexTitle from "../components/indexTitle"
+import "../styles/index.css"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <IndexTitle />
     <p>Software engineer interested in application development</p>
-    <Link to="/about/">Go to about</Link> <br />
+    <Link to="/work"><button>See My Work 🡒</button></Link>
   </Layout>
 )
 
