f7a7abca440591b92a374a861bd62ffb1dcf9ff6
Eric Weischedel <eweischedel@scheduleengine.com>
2020-07-06 12:55:45 -0400

    changes

--- src/components/header.js
+++ src/components/header.js
@@ -8,37 +8,23 @@ const activeLinkStyles = {
 }
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `white`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      className="header"
-      style={{
-        margin: `0 auto`,
-        maxWidth: 1440,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `black`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-      <ul>
-        <li><Link to="/work" activeStyle={activeLinkStyles}>Work</Link></li>
-        <li><Link to="/about" activeStyle={activeLinkStyles}>About</Link></li>
-        <li><Link to="/contact" activeStyle={activeLinkStyles}>Contact</Link></li>
+  <header>
+    <h1>
+      <Link
+        to="/"
+        style={{
+          color: `black`,
+          textDecoration: `none`,
+        }}
+      >
+        {siteTitle}
+      </Link>
+    </h1>
+    <ul>
+      <li><Link to="/work" activeStyle={activeLinkStyles}>Work</Link></li>
+      <li><Link to="/about" activeStyle={activeLinkStyles}>About</Link></li>
+      <li><Link to="/contact" activeStyle={activeLinkStyles}>Contact</Link></li>
       </ul>
-    </div>
   </header>
 )
 
