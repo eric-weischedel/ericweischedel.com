185551a84ee5ea67362a65863e60a90a86e25ccb
Eric Weischedel <yshuttle@gmail.com>
2020-07-07 22:11:31 -0400

    Random changes

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,14 @@
 import React from "react"
 import "../styles/footer.css"
+import { GitHub, Linkedin, Paperclip } from "react-feather"
+
 
 const Footer = () => (
   <footer>
     <ul>
-      <li><a href="https:/github.com/eric-weischedel">GitHub</a></li>
-      <li><a href="https:/linkedin.com/in/eric-weischedel">LinkedIn</a></li>
-      <li><a href="mailto:yshuttle@gmail.com">Resume</a></li>
+      <li><a href="https:/github.com/eric-weischedel"><GitHub color="#000" size={22} /><span>GitHub</span></a></li>
+      <li><a href="https:/linkedin.com/in/eric-weischedel"><Linkedin color="#000" size={22} /><span>LinkedIn</span></a></li>
+      <li><a href="mailto:yshuttle@gmail.com"><Paperclip color="#000" size={22} /><span>Resume</span></a></li>
     </ul>
     <p>Eric Weischedel</p>
     <p>yshuttle@gmail.com</p>
