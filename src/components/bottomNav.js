cfd6c8a36247a226f2a1ab1edbd32876bd0b34f0
Eric Weischedel <eweischedel@scheduleengine.com>
2020-07-08 11:17:49 -0400

    Get nav bar active links working

--- src/components/bottomNav.js
+++ src/components/bottomNav.js
@@ -1,33 +1,14 @@
 import React from "react"
-import { Link } from "gatsby"
-import "../styles/BottomNav.css"
-import { Briefcase, Smile, Send } from "react-feather"
+import NavItem from "./navItem.js"
+import "../styles/bottomNav.css"
 
-const activeLinkStyles = {
-  color: "blue",
-  fontFamily: "Roboto-Black",
-}
-
-const BottomNav = () => (
-  <div className="bottomNav hidden">
-    <Link className="navItem" to="/work">
-      <span>
-        <Briefcase color="#666" size={22} />
-      </span>
-      <span>Work</span>
-    </Link>
-    <Link className="navItem" to="/about">
-      <span>
-        <Smile color="#666" size={22} />
-      </span>
-      <span>About</span>
-    </Link>
-    <Link className="navItem" to="/contact">
-      <span>
-        <Send color="#666" size={22} />
-      </span>
-      <span>Contact</span>
-    </Link>
-  </div>
-)
+const BottomNav = () => {
+  return (
+    <div className="bottomNav hidden">
+      <NavItem label="Work" path="/work"/>
+      <NavItem label="About" path="/about"/>
+      <NavItem label="Contact" path="/contact"/>
+    </div>
+  );
+};
 export default BottomNav
---
+++ src/components/navItem.js
@@ -0,0 +1,32 @@
+import React from "react"
+import { Link } from "gatsby"
+import { Briefcase, Smile, MessageCircle } from "react-feather"
+import { primary } from "../styles/colors.js"
+import "../styles/navItem.css"
+
+
+const NavItem = ({ label, path }) => {
+  var color = "#666666";
+  var active = false;
+  if (window.location) {
+    active = window.location.pathname === path;
+    color = active ? primary : "#666666";
+  }
+  var icon;
+  if (label === "Work") {
+    icon = <Briefcase color={color} size={22} />;
+  } else if (label === "About") {
+    icon = <Smile color={color} size={22} />;
+  } else if (label === "Contact") {
+    icon = <MessageCircle color={color} size={22} />;
+  }
+  return (
+    <Link className="navItem" to={path}>
+      <span>
+        {icon}
+      </span>
+      <span className={active ? "active" : "inactive"}>{label}</span>
+    </Link>
+  );
+};
+export default NavItem
