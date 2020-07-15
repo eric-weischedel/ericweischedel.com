8cbb7b0c190131532f2627f6fd8dea8997c8a84c
Eric Weischedel <eweischedel@scheduleengine.com>
2020-07-15 16:33:58 -0400

    Add alternating column layouts

--- src/pages/work.js
+++ src/pages/work.js
@@ -27,6 +27,22 @@ const WorkPage = () => (
         }
       ]}
     />
+    <WorkEntry
+      title="Email List Filtering Widget"
+      subtitle="Listrak, Inc."
+      desc={["Listrak is an eCommerce marketing automation platform and CRM. The platform includes a filter tool for B2B Enterprise SaaS clients to define audiences. The purpose of this project was to create an improved filter tool. I worked with a team of developers in an Agile Scrum environment.", "Technologies used included .NET Framework, C#, JavaScript, Vue.js, HTML/CSS, SQL Server, Git, and the Atlassian software suite."]}
+      img={Listrak}
+      links={[
+        {
+          title: "Learn More About the Filter",
+          url: "https://help.listrak.com/en/articles/3951597-introducing-segment-filter-2-0-beta"
+        },
+        {
+          title: "View App",
+          url: "http://listrak.com"
+        }
+      ]}
+    />
   </Layout>
 )
 
