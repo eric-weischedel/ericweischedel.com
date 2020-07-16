02b5a448db45d6e8e88cf418d02371474082b59c
Eric Weischedel <eweischedel@scheduleengine.com>
2020-07-16 14:46:12 -0400

    Add vector to index

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,13 +6,10 @@ import { GitHub, Linkedin, Paperclip } from "react-feather"
 const Footer = () => (
   <footer>
     <ul>
-      <li><a href="https:/github.com/eric-weischedel"><GitHub color="#000" size={22} /><span>GitHub</span></a></li>
-      <li><a href="https:/linkedin.com/in/eric-weischedel"><Linkedin color="#000" size={22} /><span>LinkedIn</span></a></li>
-      <li><a href="mailto:yshuttle@gmail.com"><Paperclip color="#000" size={22} /><span>Resume</span></a></li>
+      <li><a href="https:/github.com/eric-weischedel"><GitHub color="#fff" size={22} /><span>GitHub</span></a></li>
+      <li><a href="https:/linkedin.com/in/eric-weischedel"><Linkedin color="#fff" size={22} /><span>LinkedIn</span></a></li>
+      <li><a href="mailto:yshuttle@gmail.com"><Paperclip color="#fff" size={22} /><span>Resume</span></a></li>
     </ul>
-    <p>Eric Weischedel</p>
-    <p>yshuttle@gmail.com</p>
-    <p>+1 717-380-2757</p>
   </footer>
 )
 export default Footer
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,16 +5,22 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import IndexTitle from "../components/indexTitle"
 import "../styles/index.css"
+import Vector from "../images/undraw_source_code_xx2e.svg"
 
 const IndexPage = () => (
   <Layout>
+    <SEO title="Home" />
     <div className="container">
-      <SEO title="Home" />
-      <IndexTitle />
-      <p className="homeText">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa nunc tristique et est suspendisse volutpat volutpat lacus. Massa nunc tristique et est suspendisse volutpat volutpat lacus.
+      <div className="textPanel">
+        <IndexTitle />
+        <p className="homeText">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa nunc tristique et est suspendisse volutpat volutpat lacus. Massa nunc tristique et est suspendisse volutpat volutpat lacus.
       </p>
-      <Link to="/work"><button className="cta">See My Work 🡒</button></Link>
+        <Link to="/work"><button className="cta">See My Work 🡒</button></Link>
+      </div>
+      <div className="imagePanel">
+        <img src={Vector} />
+      </div>
     </div>
   </Layout>
 )
