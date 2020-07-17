2e5b98d43befd39ba76595b3cc3248fa11d41d73
Eric Weischedel <eweischedel@scheduleengine.com>
2020-07-17 13:27:02 -0400

    Add second vector to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,13 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import IndexTitle from "../components/indexTitle"
-import Vector from "../images/undraw_source_code_xx2e.svg"
+import Hero1 from "../images/undraw_source_code_xx2e.svg"
+import Hero2 from "../images/undraw_solution_mindset_34bi.svg"
+
+function getRandomHero() {
+  const idx = Math.floor(Math.random() * 2);
+  return [Hero1, Hero2][idx]
+}
 
 const IndexPage = () => (
   <Layout>
@@ -13,12 +19,16 @@ const IndexPage = () => (
       <div className="textPanel">
         <IndexTitle />
         <p className="homeText">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa nunc tristique et est suspendisse volutpat volutpat lacus. Massa nunc tristique et est suspendisse volutpat volutpat lacus.
-      </p>
-        <Link to="/work"><button className="cta">See My Work 🡒</button></Link>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa nunc
+          tristique et est suspendisse volutpat volutpat lacus. Massa nunc
+          tristique et est suspendisse volutpat volutpat lacus.
+        </p>
+        <Link to="/work">
+          <button className="cta">See My Work 🡒</button>
+        </Link>
       </div>
       <div className="imagePanel">
-        <img src={Vector} />
+        <img src={getRandomHero()} />
       </div>
     </div>
   </Layout>
