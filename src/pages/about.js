e0450bc9391933a8b8d0da3926789bb4ef0572ad
Eric Weischedel <yshuttle@gmail.com>
2020-07-18 14:22:39 -0400

    changes

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,12 +3,18 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import PageTitle from "../components/pageTitle"
+import SecondaryAction from "../components/secondaryAction"
 
 const AboutPage = () => (
   <Layout>
     <SEO title="About Me" />
-    <PageTitle title="About Me"/>
-    <p>Hi, I'm Eric</p>
+    <div className="aboutContainer">
+      <PageTitle title="About Me"/>
+      <div className="seeMore">
+        <span className="promptText">Interested in reaching out?</span>
+        <SecondaryAction text="Get In Touch" link="/contact" />
+      </div>
+    </div>
   </Layout>
 )
 
