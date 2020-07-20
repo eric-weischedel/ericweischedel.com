864c0ccc649aad26f62459c599a1a5532983f04f
Eric Weischedel <eweischedel@scheduleengine.com>
2020-07-20 10:52:24 -0400

    Modify about page

---
+++ src/components/gatsbyImages/ericImg.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+const Image = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "eric.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 1440) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `);
+
+  const style = {
+    borderRadius: "1em"
+  }
+
+  return <Img fluid={data.placeholderImage.childImageSharp.fluid} style={style} />
+}
+
+export default Image
--- src/pages/about.js
+++ src/pages/about.js
@@ -5,19 +5,30 @@ import SEO from "../components/seo"
 import PageTitle from "../components/pageTitle"
 import SecondaryAction from "../components/secondaryAction"
 
-import ListrakImg from "../components/gatsbyImages/listrakImg"
+import EricImg from "../components/gatsbyImages/ericImg"
 
 const AboutPage = () => (
   <Layout>
     <SEO title="About Me" />
+    <PageTitle title="About Me" />
     <div className="aboutContainer">
-      <PageTitle title="About Me"/>
-
-      <ListrakImg />
-
-      <div className="seeMore">
-        <span className="promptText">Interested in reaching out?</span>
-        <SecondaryAction text="Get In Touch" link="/contact" />
+      <div className="textPanel">
+        <div className="aboutSummary">
+          <h2 className="heading">My Background</h2>
+          <p className="paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa nunc tristique et est suspendisse volutpat volutpat lacus. Massa nunc tristique et est suspendisse volutpat volutpat lacus. Amet sapien ac leo aliquam donec libero tellus nisl. Dignissim eu vivamus condimentum mattis lacinia. Sed risus vitae elementum elit viverra pulvinar lacus, porta.</p>
+          <p className="paragraph">Massa nunc tristique et est suspendisse volutpat volutpat lacus. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <br />
+          <h2 className="heading">What I Offer</h2>
+          <p className="paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa nunc tristique et est suspendisse volutpat volutpat lacus. Massa nunc tristique et est suspendisse volutpat volutpat lacus. Amet sapien ac leo aliquam donec libero tellus nisl. Dignissim eu vivamus condimentum mattis lacinia. Sed risus vitae elementum elit viverra pulvinar lacus, porta. Massa nunc tristique et est suspendisse volutpat volutpat lacus. </p>
+          <br />
+        </div>
+        <div className="seeMore">
+          <span className="promptText">Interested in reaching out?</span>
+          <SecondaryAction text="Get In Touch" link="/contact" />
+        </div>
+      </div>
+      <div className="imagePanel">
+        <EricImg />
       </div>
     </div>
   </Layout>
