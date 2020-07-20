829c0132128724ca06195e205e3604e4f91de9bd
Eric Weischedel <eweischedel@scheduleengine.com>
2020-07-20 10:57:58 -0400

    Modify work entry component

--- src/components/workEntry.js
+++ src/components/workEntry.js
@@ -5,14 +5,15 @@ import SecondaryAction from "./secondaryAction"
 const WorkEntry = ({ children, title, subtitle, img, desc, links, badgeImages = [] }) => (
   <article className="workEntry">
     <div className="workEntryTextPanel">
-      <h2 className="workEntryTitle">{title}</h2>
+      <h2 className="heading">{title}</h2>
       {subtitle &&
         <h3 className="workEntrySubtitle">{subtitle}</h3>
       }
       <Badges images={badgeImages} />
-      <div className="workEntryDescription">
+      <br />
+      <div>
         {desc.map((paragraph) =>
-          <div><p className="workEntryDescriptionParagraph">{paragraph}</p><br /></div>
+          <div><p className="paragraph">{paragraph}</p><br /></div>
         )}
       </div>
       {links.map((link) =>
