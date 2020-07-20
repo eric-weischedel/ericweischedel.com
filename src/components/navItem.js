22e41e09c362900968d387784b69160d6d845c7b
Eric Weischedel <eweischedel@scheduleengine.com>
2020-07-20 14:52:42 -0400

    Change mobile active link to not use window

--- src/components/navItem.js
+++ src/components/navItem.js
@@ -4,12 +4,10 @@ import { Briefcase, Smile, MessageCircle } from "react-feather"
 import { primary } from "../styles/colors.js"
 
 const NavItem = ({ label, path }) => {
-  var color = "#666666";
-  var active = false;
-  if (window) {
-    active = window.location.pathname === path;
-    color = active ? primary : "#666666";
-  }
+  const isActive = ({ isCurrent }) => {
+    return isCurrent ? { className: "navItem active" } : {};
+  };
+  const color = "#666666";
   var icon;
   if (label === "Work") {
     icon = <Briefcase color={color} size={22} strokeWidth={2} />;
@@ -19,11 +17,11 @@ const NavItem = ({ label, path }) => {
     icon = <MessageCircle color={color} size={22} strokeWidth={2} />;
   }
   return (
-    <Link className="navItem" to={path}>
+    <Link getProps={isActive} className="navItem" to={path}>
       <span>
         {icon}
       </span>
-      <span className={active ? "active" : "inactive"}>{label}</span>
+      <span>{label}</span>
     </Link>
   );
 };
