c41ee0f967317fb2bc2b625391533689a3bb2112
Eric Weischedel <eweischedel@scheduleengine.com>
2020-07-21 17:00:51 -0400

    Add custom not found page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,17 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import PageTitle from "../components/pageTitle"
+
+import Hero from "../images/undraw_not_found_60pq.svg"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <PageTitle title="Not Found" />
+    <div className="notFoundContainer">
+      <img src={Hero} alt="" />
+    </div>
   </Layout>
 )
 
