9c9a1416932feeb2288ec69e95512277077e513d
Eric Weischedel <yshuttle@gmail.com>
2020-07-21 21:05:56 -0400

    About text

--- src/pages/about.js
+++ src/pages/about.js
@@ -15,13 +15,25 @@ const AboutPage = () => (
       <div className="textPanel">
         <div className="aboutSummary">
           <h2 className="heading">My Background</h2>
-          <p className="paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa nunc tristique et est suspendisse volutpat volutpat lacus. Massa nunc tristique et est suspendisse volutpat volutpat lacus. Amet sapien ac leo aliquam donec libero tellus nisl. Dignissim eu vivamus condimentum mattis lacinia. Sed risus vitae elementum elit viverra pulvinar lacus, porta.</p>
-          <p className="paragraph">Massa nunc tristique et est suspendisse volutpat volutpat lacus. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <p className="paragraph">
+            In 1998, I was born in <em>Lancaster, Pennsylvania,</em> where I grew up and live today.
+          </p>
+          <p className="paragraph">
+            At a young age I gained an interest in computers through playing video games and solving IT problems for my family. <b>I first dabbled into programming on my TI-84 graphing calculator</b> in highschool. I really enjoyed the logic behind it.
+          </p>
+          <p className="paragraph">
+            After graduating high school in 2017, I decided to major in Computer Science at <em>Messiah University.</em> While there, I loved my programming classes, especially a web development class where we built a full-stack app from scratch. <b>I soon decided I would pursue a career in software engineering.</b>
+          </p>
+          <p className="paragraph">
+            To that end, I began working as an Application Developer Intern for <em>Listrak</em>. I had an amazing time there and learned a ton about the software industry. This past summer, I began working as a Backend Developer Intern for <em>HomeX.</em> The experience continually expands my horizons and increases my <b>passion for software engineering.</b>
+          </p>
           <br />
-          <h2 className="heading">What I Offer</h2>
-          <p className="paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa nunc tristique et est suspendisse volutpat volutpat lacus. Massa nunc tristique et est suspendisse volutpat volutpat lacus. Amet sapien ac leo aliquam donec libero tellus nisl. Dignissim eu vivamus condimentum mattis lacinia. Sed risus vitae elementum elit viverra pulvinar lacus, porta. Massa nunc tristique et est suspendisse volutpat volutpat lacus. </p>
-          <br />
+          <h2 className="heading">What I'm Up To</h2>
+          <p className="paragraph">
+            Currently, I am working as a Backend Developer Intern for <em>HomeX.</em> This Fall, I will begin my final year at <em>Messiah University,</em> where I will attain a Bachelor of Science in Computer Science. I am seeking job opportunities for after graduation.
+          </p>
         </div>
+        <br />
         <div className="seeMore">
           <span className="promptText">Interested in reaching out?</span>
           <SecondaryAction text="Contact Me" link="/contact" />
